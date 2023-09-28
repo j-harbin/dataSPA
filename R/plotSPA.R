a7113f9b6dbf453c83c6ac3e1e8a61d8daf16de4
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2023-09-28 12:49:24 -0300

    legends

--- R/plotSPA.R
+++ R/plotSPA.R
@@ -488,8 +488,8 @@ plotSPA <-
           ylim = c(0, max(unlist(ylim))*1.5),
           xlab = " ",
           las=2,
-          legend.text = TRUE,
-          args.legend=list(x="bottomright", inset=c(-0.35,0), cex=0.7)
+          legend.text = TRUE, #JAIM
+          args.legend=list(x="bottomright", legend=wrapText(string=sort(namesFunding)), inset=c(-0.35,0), cex=0.7, bty="n")
         )
         title(ylab = "Amount of O&M Funding ($)", mgp = c(4, 1, 0))
         combine <- c(unlist(unname(ms)), unlist(unname(dv)))
@@ -644,7 +644,7 @@ plotSPA <-
             )
           legend(
             "topleft",
-            c(rownames(DFs)[-(which(grepl("GAP", rownames(DFs))))], "Gap in funding"),
+            wrapText(c(rownames(DFs)[-(which(grepl("GAP", rownames(DFs))))], "Gap in funding")),
             col = c(j[which(!(j == "red"))], "red"),
             pch = rep(20, (length(namesFunding)+1)),
             cex = 0.7
@@ -1104,9 +1104,8 @@ plotSPA <-
           xlab = " ",
           las=2,
           legend.text = TRUE,
-          args.legend=list(x="bottomright", inset=c(-0.35,0), cex=0.7))
+          args.legend=list(x="bottomright", legend=wrapText(sort(salnamesFunding)), inset=c(-0.35,0), cex=0.7),bty="n")
         } else {
-          # JAIMIE LEE
           if (length(salyears) > 6) {
           holder <- data.frame(matrix(0, nrow = length(saldf[,1]), 1))
           names(holder) <- c("  ")
@@ -1132,7 +1131,7 @@ plotSPA <-
             ylim = c(0, max(unlist(ylim))*2),
             legend.text = TRUE,
             xlab = " ",
-            args.legend=list(x="topright", inset=c(-0.35,0), cex=0.5),
+            args.legend=list(x="topright", legend=wrapText(sort(salnamesFunding)),inset=c(-0.35,0), cex=0.5,bty="n"),
             las=2
           )
 
@@ -1966,10 +1965,11 @@ bp <-
   )
 legend(
   "topleft",
-  c(rownames(dfROI2)[-(which(grepl("GAP", rownames(dfROI2))))], "Gap in funding"),
+  wrapText(c(rownames(dfROI2)[-(which(grepl("GAP", rownames(dfROI2))))], "Gap in funding")),
   col = c(j[which(!(j == "red"))], "red"),
   pch = rep(20, (length(salnamesFunding)+1)),
-  cex = 0.7
+  cex = 0.7,
+  bty="n"
 )
 } else {
 
--- R/wrapText.R
+++ R/wrapText.R
@@ -1,5 +1,5 @@
 #' Create new line for long string
-
+#'
 #' This function identifies which strings have charaters greater than
 #' the specified `nchar` value. If the string is greater than the
 #' specified `nchar`, it checks if the nchar(th) character is a space.
@@ -8,11 +8,15 @@
 #' in legends when funding source names are longer than desired.
 #'
 #' @param string a character string or list of character strings
+#' to be dived by nchar times
 #' @param nchar the number indicating the position to create a
-#'new line in a string
+#' new line in a string
 #' @param debug integer value indicating level of debugging.
 #'  If this is less than 1, no debugging is done. Otherwise,
 #'  some functions will print debugging information.
+#'
+#' @return a string with line breaks every nchar characters
+
 #' @examples
 #' \dontrun{
 #'  # 1. Try a string less than 20 characters
@@ -29,10 +33,12 @@
 #' and there is no space after
 #' cat(wrapText("ABCDEF GHIJHLMOPDSDABBBBAAAASASASJH"))
 #' }
+#' @author Jaimie Harbin
+#' @export
 
 wrapText <- function(string, nchar=20, debug=0) {
   if (debug > 0) {
-    message("string is ", paste0(string, collapse=","), " and nchar is ", nchar)
+    message("string is ", paste0(string, collapse=","), " and nchar is ", nchar, '. The length of string is ', length(string))
   }
   final <- NULL
   for (i in seq_along(string)) {
@@ -41,6 +47,7 @@ wrapText <- function(string, nchar=20, debug=0) {
       if (debug > 0) {
         message("string ", string[i], " has more than ", nchar, " characters" )
       }
+      #browser()
       match <- substr(lab, nchar, nchar)
       if (match == " ") { # If the nchar value is not a space
           # split the label here
@@ -63,7 +70,7 @@ wrapText <- function(string, nchar=20, debug=0) {
               message("A space was found after the nchar, which was not a space")
             }
             # Extract the position of the first space
-            first_space_position <- match_positions[[1]][[1]] + 20
+            first_space_position <- match_positions[[1]][[1]] + nchar
             newString <- paste0(substring(lab, 1, first_space_position), "\n", substring(lab, (first_space_position+1), nchar(lab)))
             final[[i]] <- newString
           } else {
@@ -83,5 +90,69 @@ wrapText <- function(string, nchar=20, debug=0) {
       final[[i]] <- lab
     }
   }
-  return(unlist(final))
+  final2 <- NULL
+  for (i in seq_along(final)) {
+    if (nchar(final[[i]]) > nchar) {
+    lab <- strsplit(final[[i]], "\n")[[1]][2]
+    } else {
+      lab <- string[[i]]
+    }
+    if (nchar(lab) > nchar) { # If more than nchar characters
+      if (debug > 0) {
+        message("string ", lab[i], " has more than ", nchar, " characters (round 2)" )
+      }
+      #browser()
+      match <- substr(lab, nchar, nchar)
+      if (match == " ") { # If the nchar value is not a space
+        # split the label here
+        if (debug > 0) {
+          message("The ", nchar, "th character for ", lab[i], " is a space (round 2)")
+        }
+        newString <- paste0(substring(lab, 1, nchar), "\n", substring(lab, (nchar+1), nchar(lab)))
+        final2[[i]] <- newString
+      } else {
+        if (debug > 0) {
+          message("The ", nchar, "th character for ", string[i], " was not a space. It was ", match, " (round 2)")
+        }
+        # split at the next space
+        # Find the first space after the 20th character
+        reg <- paste0(".{",nchar,"}\\s")
+        match_positions <- gregexpr(reg, lab)
+        # Check if a match was found
+        if (match_positions[[1]][[1]] >= 0) {
+          if (debug > 0) {
+            message("A space was found after the nchar, which was not a space (round 2)")
+          }
+          # Extract the position of the first space
+          first_space_position <- match_positions[[1]][[1]] + nchar
+          newString <- paste0(substring(lab, 1, first_space_position), "\n", substring(lab, (first_space_position+1), nchar(lab)))
+          final2[[i]] <- newString
+        } else {
+          if (debug > 0) {
+            message("It was necessary to find a space before nchar (round 2)")
+          }
+          # We reached the end, so split at the space before 20th character
+          # Find the space right before the 20th character
+          sub<- substr(lab, 1, (nchar-1))
+          space <- max(grep(" ", strsplit(sub, "")[[1]]))
+          newString <- paste0(substring(lab, 1, space), "\n", substring(lab, (space+1), nchar(lab)))
+          final2[[i]] <- newString
+        }
+      }
+    } else {
+      # Label wasn't too long
+      final2[[i]] <- lab
+    }
+  }
+  # Determine if we need to split again
+final3 <- NULL
+for (i in seq_along(final)) {
+  if (!(identical(final[[i]], final2[[i]]))) {
+    final3[[i]] <- paste(substr(final[[i]], start = 1, stop = regexpr("\n", final[[i]]) - 1), paste0("\n", final2[[i]]))
+  } else {
+    final3[[i]] <- final[[i]]
+  }
+}
+#browser()
+  return(unlist(final3))
 }
