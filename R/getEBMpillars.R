66ee833bd7fca9fdce4843dad2049a9fdb28fb09
Remi Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2023-12-21 15:15:33 -0400

    create getEBMpillars function

---
+++ R/getEBMpillars.R
@@ -0,0 +1,194 @@
+#' Get the discriminating words used to identify EBM pillars or objectives.
+#'
+#' Provides words and colours to be used in highlighting.
+#'
+#' @param file the word file describing the EBM framwork. Defaults to the IN folder
+#' @importFrom officer read_docx
+#' @importFrom officer docx_summary
+#' @importFrom tidyr separate_wider_delim
+#' @importFrom dplyr mutate
+#' @importFrom dplyr group_by
+#' @importFrom desc description
+#' @importFrom dplyr anti_join
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr summarise
+#' @importFrom dplyr right_join
+#' @importFrom tidyr separate_wider_delim
+#' @importFrom tidytext unnest_tokens
+#' @importFrom stringi stri_replace_all_regex
+#' @importFrom tidytext get_stopwords
+#' @importFrom dplyr filter
+#' @importFrom stringr word
+#' @importFrom colorspace lighten
+#' @importFrom colorspace darken
+#' @importFrom dplyr n
+#' @importFrom dplyr if_else
+#' @export
+#' @author Remi Daigle and Jaimie Harbin
+getEBMpillars <- function(file="//dcnsbiona01a/BIODataSvc/IN/MSP/PowerBI-Projects/dataSPA/inputs/Stephenson_ebfm.R") {
+
+  if(endsWith(file,"docx")){
+    message("You are using Bundy et al. (unpublished) for pillar definitions")
+    subpoint <- words <-  color <- NULL
+
+    if (file.exists(file)) {
+      message("You are using Bundy et al. (unpublished) for pillar definitions")
+      docx <- (read_docx(file) %>%
+                 docx_summary())[-c(1:4,18),]
+    } else {
+      stop("You are either not on the VPN, or do not have access to the IN folder needed for this function")
+    }
+
+    pillars <- data.frame()
+    for(i in seq_along(docx$text)){
+      # if(i==47) browser()
+      if(is.na(docx$level[i])) {
+        pillar <- docx$text[i]
+      }else if(docx$level[i]==1){
+        objective <- docx$text[i]
+      }else if(docx$level[i]==2){
+        point <- docx$text[i]
+        if(docx$level[i]>=docx$level[i+1]){
+          pillars <- rbind(pillars,
+                           data.frame(pillar=pillar,
+                                      objective=objective,
+                                      point=point,
+                                      subpoint="",
+                                      level=docx$level[i]))
+        }
+      }else if(docx$level[i]>2){
+        # browser()
+        pillars <- rbind(pillars,
+                         data.frame(pillar=pillar,
+                                    objective=objective,
+                                    point=point,
+                                    subpoint=docx$text[i],
+                                    level=docx$level[i]))
+      }
+
+    }
+
+    pillars <- pillars %>%
+      separate_wider_delim(pillar,":",names=c("pillar","description"))%>%
+      mutate(pillar=gsub(".. ","",pillar))
+
+
+    objectives <- pillars %>%
+      mutate(objective=paste0(pillar,": ",objective)) %>%
+      group_by(pillar,description,objective) %>%
+      summarise(words=paste(point,subpoint,collapse=" "),
+                .groups = 'drop') %>%
+      group_by(pillar,description,objective) %>%
+      unnest_tokens(word,words) %>%
+      anti_join(get_stopwords(),by="word") %>%
+      filter(!nchar(word)<=3) %>%
+      group_by(pillar,description,objective,word) %>%
+      summarise(n=n(),
+                .groups = 'drop') %>%
+      ungroup()
+
+
+    pal <- c(lighten("#1b9e77",0.4),
+             lighten("#1b9e77",0.1),
+             darken("#1b9e77",0.1),
+             darken("#1b9e77",0.4),
+             lighten("#7570b3",0.4),
+             "#7570b3",
+             darken("#7570b3",0.4),
+             lighten("#d95f02",0.2),
+             darken("#d95f02",0.2))
+
+
+    objectives_col <- objectives %>%
+      right_join(data.frame(objective=as.factor(unique(objectives$objective)),
+                            color=pal),
+                 by="objective")%>%
+      group_by(word) %>%
+      summarise(hl_word=if_else(n()==1,
+                                paste0("<span style='background-color: ",color, "'>", word, "</span>",collapse = ""),
+                                lapply(seq(length(color)),function(c){
+                                  seg <- length(word)
+                                  len <- nchar(unique(word))
+                                  len %/% seg
+                                  if(c==1){
+                                    paste0("<span style='background-color: ",color[c], "'>",
+                                           substr(unique(word),1,len %/% seg + len %% seg),
+                                           "</span>",collapse = "")
+                                  } else {
+                                    paste0("<span style='background-color: ",color[c], "'>",
+                                           substr(unique(word),
+                                                  (len %/% seg)*(c-1) + len %% seg +1,
+                                                  (len %/% seg)*(c-1) + len %% seg + len %/% seg),
+                                           "</span>",collapse = "")
+                                  }
+                                }) %>%
+                                  unlist() %>%
+                                  paste0(collapse = "")
+      )
+      )
+  } else if(endsWith(file,"R")){
+
+    if (file.exists(file)) {
+      message("You are using Stephenson et al. (unpublished) for pillar definitions")
+      source(file)
+      pillars <- TABLE3 %>%
+        separate(table3,sep="---",into = c("pillar","objective","subobj","indicator"))
+
+      objectives <- pillars %>%
+        group_by(pillar) %>%
+        summarise(words=paste(objective,subobj,indicator,collapse=" "),
+                  .groups = 'drop') %>%
+        group_by(pillar) %>%
+        unnest_tokens(word,words) %>%
+        anti_join(get_stopwords(),by="word") %>%
+        filter(!nchar(word)<=3) %>%
+        group_by(pillar,word) %>%
+        summarise(n=n(),
+                  .groups = 'drop') %>%
+        ungroup()
+
+      pal <- c("#7fc97f",
+               "#beaed4",
+               "#fdc086",
+               "#ffff99")
+
+      objectives_col <- objectives %>%
+        right_join(data.frame(pillar=as.factor(unique(objectives$pillar)),
+                              color=pal),
+                   by="pillar")%>%
+        group_by(word) %>%
+        summarise(hl_word=if_else(n()==1,
+                                  paste0("<span style='background-color: ",color, "'>", word, "</span>",collapse = ""),
+                                  lapply(seq(length(color)),function(c){
+                                    seg <- length(word)
+                                    len <- nchar(unique(word))
+                                    len %/% seg
+                                    if(c==1){
+                                      paste0("<span style='background-color: ",color[c], "'>",
+                                             substr(unique(word),1,len %/% seg + len %% seg),
+                                             "</span>",collapse = "")
+                                    } else {
+                                      paste0("<span style='background-color: ",color[c], "'>",
+                                             substr(unique(word),
+                                                    (len %/% seg)*(c-1) + len %% seg +1,
+                                                    (len %/% seg)*(c-1) + len %% seg + len %/% seg),
+                                             "</span>",collapse = "")
+                                    }
+                                  }) %>%
+                                    unlist() %>%
+                                    paste0(collapse = "")
+        )
+        )
+
+      #going to try to limit the words to the "best" words in a later version
+      # test <- objectives %>%
+      #   bind_tf_idf(word,pillar,n) %>%
+      #   arrange(desc(tf_idf)) %>%
+      #   group_by(pillar) %>%
+      #   slice_max(tf_idf,n=30)
+
+    } else {
+      stop("You are either not on the VPN, or do not have access to the IN folder needed for this function")
+    }
+  }
+}
--- R/highlightOverview.R
+++ R/highlightOverview.R
@@ -32,7 +32,7 @@
 
 highlightOverview <-
   function(om = NULL,salary = NULL,id = NULL,legend = FALSE,
-file="//dcnsbiona01a/BIODataSvc/IN/MSP/PowerBI-Projects/dataSPA/inputs/Ecological Pillar Objectives 20 April 2023.docx") {
+file="//dcnsbiona01a/BIODataSvc/IN/MSP/PowerBI-Projects/dataSPA/inputs/Stephenson_ebfm.R") {
 
   if (is.null(om) && is.null(salary)) {
     stop("In highlightOverview must provide an om or salary argument, likely from getData()")
@@ -45,103 +45,7 @@ file="//dcnsbiona01a/BIODataSvc/IN/MSP/PowerBI-Projects/dataSPA/inputs/Ecologica
     om <- salary
   }
 
-  subpoint <- words <-  color <- NULL
-
-  if (file.exists(file)) {
-  docx <- (read_docx(file) %>%
-             docx_summary())[-c(1:4,18),]
-  } else {
-    stop("You are either not on the VPN, or do not have access to the IN folder needed for this function")
-  }
-
-  pillars <- data.frame()
-  for(i in seq_along(docx$text)){
-    # if(i==47) browser()
-    if(is.na(docx$level[i])) {
-      pillar <- docx$text[i]
-    }else if(docx$level[i]==1){
-      objective <- docx$text[i]
-    }else if(docx$level[i]==2){
-      point <- docx$text[i]
-      if(docx$level[i]>=docx$level[i+1]){
-        pillars <- rbind(pillars,
-                         data.frame(pillar=pillar,
-                                    objective=objective,
-                                    point=point,
-                                    subpoint="",
-                                    level=docx$level[i]))
-      }
-    }else if(docx$level[i]>2){
-      # browser()
-      pillars <- rbind(pillars,
-                       data.frame(pillar=pillar,
-                                  objective=objective,
-                                  point=point,
-                                  subpoint=docx$text[i],
-                                  level=docx$level[i]))
-    }
-
-  }
-
-  pillars <- pillars %>%
-    separate_wider_delim(pillar,":",names=c("pillar","description"))%>%
-    mutate(pillar=gsub(".. ","",pillar))
-
-
-  objectives <- pillars %>%
-    mutate(objective=paste0(pillar,": ",objective)) %>%
-    group_by(pillar,description,objective) %>%
-    summarise(words=paste(point,subpoint,collapse=" "),
-              .groups = 'drop') %>%
-    group_by(pillar,description,objective) %>%
-    unnest_tokens(word,words) %>%
-    anti_join(get_stopwords(),by="word") %>%
-    filter(!nchar(word)<=3) %>%
-    group_by(pillar,description,objective,word) %>%
-    summarise(n=n(),
-              .groups = 'drop') %>%
-    ungroup()
-
-
-  pal <- c(lighten("#1b9e77",0.4),
-           lighten("#1b9e77",0.1),
-           darken("#1b9e77",0.1),
-           darken("#1b9e77",0.4),
-           lighten("#7570b3",0.4),
-           "#7570b3",
-           darken("#7570b3",0.4),
-           lighten("#d95f02",0.2),
-           darken("#d95f02",0.2))
-
-
-  objectives_col <- objectives %>%
-    right_join(data.frame(objective=as.factor(unique(objectives$objective)),
-                          color=pal),
-               by="objective")%>%
-    group_by(word) %>%
-    summarise(hl_word=if_else(n()==1,
-                              paste0("<span style='background-color: ",color, "'>", word, "</span>",collapse = ""),
-                              lapply(seq(length(color)),function(c){
-                                seg <- length(word)
-                                len <- nchar(unique(word))
-                                len %/% seg
-                                if(c==1){
-                                  paste0("<span style='background-color: ",color[c], "'>",
-                                         substr(unique(word),1,len %/% seg + len %% seg),
-                                         "</span>",collapse = "")
-                                } else {
-                                  paste0("<span style='background-color: ",color[c], "'>",
-                                         substr(unique(word),
-                                                (len %/% seg)*(c-1) + len %% seg +1,
-                                                (len %/% seg)*(c-1) + len %% seg + len %/% seg),
-                                         "</span>",collapse = "")
-                                }
-                              }) %>%
-                                unlist() %>%
-                                paste0(collapse = "")
-    )
-    )
-
+  getEBMpillars(file=file)
 
 
 keep <- unlist(unique(om$overview[which(om$project_id == id)]))
